b796ec06e621525adc33ef3af365762354162edc
dawson long <dawsonreschke@icloud.com>
2023-01-26 03:04:37 -0800

    Added not found middleware

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,10 @@
 const express = require('express'); 
 const routes = require('./routes')
+const notFound = require('./middlewares/notFoundMiddleWare')
+
 const app = express(); 
 app.use(routes)
+app.use(notFound)
 
 
 
---
+++ server/api/middlewares/notFoundMiddleWare.js
@@ -0,0 +1,4 @@
+const notFound = (req,res,next) => {
+    res.status(404); 
+    res.json({message: 'Sorry the route you have requested does not exist.'})
+}

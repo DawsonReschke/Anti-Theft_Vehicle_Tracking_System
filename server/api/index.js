ae3907caf05e61ba9a17810caf662eebdb4ab3b4
dawson long <dawsonreschke@icloud.com>
2023-01-26 19:36:38 -0800

    Moved backend routes from ..../location -> ..../api/location

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,18 @@
 const express = require('express'); 
+const path = require('path');
 const routes = require('./routes');
 const notFound = require('./middlewares/notFoundMiddleWare');
 const errorHandler = require('./middlewares/errorHandlerMiddleWare');
 
 const app = express(); 
-app.use(routes);
+app.use('/api',routes);
+
+/** 
+* GET / route 
+* Serve static files for frontend
+*/
+app.use('/',express.static(path.join(__dirname,'../../client/dist')))
+
 app.use(errorHandler);
 app.use(notFound);
 
--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,5 +1,4 @@
 const express = require('express'); 
-const path = require('path');
 
 const router = express.Router(); 
 
@@ -12,12 +11,5 @@ const location = require('./location/locationRouter')
 * Use all of the routes 
 */
 router.use('/location',location)
-
-/** 
-* GET / route 
-* Serve static files for frontend
-*/
-router.use('/',express.static(path.join(__dirname,'../../../client/dist')))
-
 
 module.exports = router;
